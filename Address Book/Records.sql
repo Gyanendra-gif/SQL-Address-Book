0b00e9a5345d91979c6ad538ecbf0af5382c2182
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-14 21:22:14 +0530

    [Gyanendra] Add: A Query to Add Table

--- Address Book/Records.sql
+++ Address Book/Records.sql
@@ -1,8 +1,8 @@
-create database address_book; --Database Creation
+create database address_book;
 use address_book;
 
 
-create table AddressBook(    -- Create Table
+create table AddressBook(
 id int identity (1,1) primary key,
 FirstName varchar(20),
 LastName varchar(20),
@@ -16,17 +16,4 @@ Email varchar(50),
 
 
 select * from AddressBook   --Display Data
-
-
-insert into AddressBook values('Gyanendra', 'Pratap', 'D12/25 New Gate', 'Rewa', 'M.P', 486001, 7474747474,'abc@gmail.com')  
-insert into AddressBook values('Mohan', 'Singh', 'D11/25 New Gate', 'Rewa', 'M.P', 486001, 7575747474,'def@gmail.com')
-insert into AddressBook values('Rohan', 'Tiwari', 'D10/26 New Gate', 'Rewa', 'M.P', 486001, 7676747474,'ghi@gmail.com')
-
-
-Update AddressBook set FirstName = 'Sohan' where FirstName = 'Gyanendra' -- Update Contact with Name
-
-
-Delete from AddressBook Where FirstName = 'Sohan' --Delete by Particular Name
-
-
 
